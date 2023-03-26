b4a8d5dfc70cd4631e0058e48b24d425803486e7
CamielK <camielk@live.nl>
2023-03-26 12:52:42 +0200

    Remove custom ranges from COCO eval

--- EVA-02/det/detectron2/evaluation/coco_evaluation.py
+++ EVA-02/det/detectron2/evaluation/coco_evaluation.py
@@ -55,7 +55,6 @@ class COCOEvaluator(DatasetEvaluator):
         use_fast_impl=True,
         kpt_oks_sigmas=(),
         allow_cached_coco=True,
-        use_custom_ranges=False,
     ):
         """
         Args:
@@ -95,8 +94,6 @@ class COCOEvaluator(DatasetEvaluator):
                 runs. You should set this to False if you need to use different validation data.
                 Defaults to True.
         """
-        self.use_custom_ranges = use_custom_ranges
-
         self._logger = logging.getLogger(__name__)
         self._distributed = distributed
         self._output_dir = output_dir
@@ -274,15 +271,13 @@ class COCOEvaluator(DatasetEvaluator):
                     use_fast_impl=self._use_fast_impl,
                     img_ids=img_ids,
                     max_dets_per_image=self._max_dets_per_image,
-                    use_custom_ranges=self.use_custom_ranges,
                 )
                 if len(coco_results) > 0
                 else None  # cocoapi does not handle empty results very well
             )
 
             res = self._derive_coco_results(
-                coco_eval, task, class_names=self._metadata.get("thing_classes"),
-                use_custom_ranges=self.use_custom_ranges
+                coco_eval, task, class_names=self._metadata.get("thing_classes")
             )
             self._results[task] = res
 
@@ -325,7 +320,7 @@ class COCOEvaluator(DatasetEvaluator):
         self._logger.info("Proposal metrics: \n" + create_small_table(res))
         self._results["box_proposals"] = res
 
-    def _derive_coco_results(self, coco_eval, iou_type, class_names=None, use_custom_ranges=False):
+    def _derive_coco_results(self, coco_eval, iou_type, class_names=None):
         """
         Derive the desired score numbers from summarized COCOeval.
 
@@ -339,28 +334,11 @@ class COCOEvaluator(DatasetEvaluator):
             a dict of {metric name: score}
         """
 
-        if use_custom_ranges:
-            metrics = {
-                "bbox": [
-                    "AP", "AP50", "AP75", "APs", "APm", "APl",
-                    "AP0-8", "AP8-16", "AP16-24", "AP24-32",
-                    "AP32-48", "AP48-64", "AP64-80", "AP80-96",
-                    "AP96-128", "AP128-160", "AP160-192", "AP192-256", "AP256-"
-                ],
-                "segm": [
-                    "AP", "AP50", "AP75", "APs", "APm", "APl",
-                    "AP0-8", "AP8-16", "AP16-24", "AP24-32",
-                    "AP32-48", "AP48-64", "AP64-80", "AP80-96",
-                    "AP96-128", "AP128-160", "AP160-192", "AP192-256", "AP256-"
-                ],
-                "keypoints": ["AP", "AP50", "AP75", "APm", "APl"],
-            }[iou_type]
-        else:
-            metrics = {
-                "bbox": ["AP", "AP50", "AP75", "APs", "APm", "APl"],
-                "segm": ["AP", "AP50", "AP75", "APs", "APm", "APl"],
-                "keypoints": ["AP", "AP50", "AP75", "APm", "APl"],
-            }[iou_type]
+        metrics = {
+            "bbox": ["AP", "AP50", "AP75", "APs", "APm", "APl"],
+            "segm": ["AP", "AP50", "AP75", "APs", "APm", "APl"],
+            "keypoints": ["AP", "AP50", "AP75", "APm", "APl"],
+        }[iou_type]
 
         if coco_eval is None:
             self._logger.warn("No predictions from the model!")
@@ -594,7 +572,6 @@ def _evaluate_predictions_on_coco(
     use_fast_impl=True,
     img_ids=None,
     max_dets_per_image=None,
-    use_custom_ranges=False,
 ):
     """
     Evaluate the coco results using COCOEval API.
@@ -623,11 +600,6 @@ def _evaluate_predictions_on_coco(
         # apply COCOevalMaxDets to evaluate AP with the custom input.
         if max_dets_per_image[2] != 100:
             coco_eval = COCOevalMaxDets(coco_gt, coco_dt, iou_type)
-
-    # todo: set coco_eval as custom coco_eval with many scale ranges
-    if use_custom_ranges:
-        coco_eval = COCOevalCustomRanges(coco_gt, coco_dt, iou_type)
-
     if iou_type != "keypoints":
         coco_eval.params.maxDets = max_dets_per_image
 
@@ -709,7 +681,7 @@ class COCOevalMaxDets(COCOeval):
         def _summarizeDets():
             stats = np.zeros((12,))
             # Evaluate AP using the custom limit on maximum detections per image
-            stats[0] = _summarize(1, maxDets=self.params.maxDets[2])  # the only thing customized is here
+            stats[0] = _summarize(1, maxDets=self.params.maxDets[2])
             stats[1] = _summarize(1, iouThr=0.5, maxDets=self.params.maxDets[2])
             stats[2] = _summarize(1, iouThr=0.75, maxDets=self.params.maxDets[2])
             stats[3] = _summarize(1, areaRng="small", maxDets=self.params.maxDets[2])
@@ -748,136 +720,3 @@ class COCOevalMaxDets(COCOeval):
 
     def __str__(self):
         self.summarize()
-
-
-class COCOevalCustomRanges(COCOeval):
-    def __init__(self, cocoGt=None, cocoDt=None, iouType='segm'):
-        '''
-        Initialize CocoEval using coco APIs for gt and dt
-        :param cocoGt: coco object with ground truth annotations
-        :param cocoDt: coco object with detection results
-        :return: None
-        '''
-        super(COCOevalCustomRanges, self).__init__(cocoGt=cocoGt, cocoDt=cocoDt, iouType=iouType)
-        # rewrite params
-        self.params = ParamsCostumRanges(iouType=iouType)  # parameters
-        if not cocoGt is None:
-            self.params.imgIds = sorted(cocoGt.getImgIds())
-            self.params.catIds = sorted(cocoGt.getCatIds())
-
-    def summarize(self):
-        '''
-        Compute and display summary metrics for evaluation results.
-        Note this functin can *only* be applied on the default parameter setting
-        '''
-        def _summarize( ap=1, iouThr=None, areaRng='all', maxDets=100 ):
-            p = self.params
-            iStr = ' {:<18} {} @[ IoU={:<9} | area={:>6s} | maxDets={:>3d} ] = {:0.3f}'
-            titleStr = 'Average Precision' if ap == 1 else 'Average Recall'
-            typeStr = '(AP)' if ap==1 else '(AR)'
-            iouStr = '{:0.2f}:{:0.2f}'.format(p.iouThrs[0], p.iouThrs[-1]) \
-                if iouThr is None else '{:0.2f}'.format(iouThr)
-
-            aind = [i for i, aRng in enumerate(p.areaRngLbl) if aRng == areaRng]
-            mind = [i for i, mDet in enumerate(p.maxDets) if mDet == maxDets]
-            if ap == 1:
-                # dimension of precision: [TxRxKxAxM]
-                s = self.eval['precision']
-                # IoU
-                if iouThr is not None:
-                    t = np.where(iouThr == p.iouThrs)[0]
-                    s = s[t]
-                s = s[:,:,:,aind,mind]
-            else:
-                # dimension of recall: [TxKxAxM]
-                s = self.eval['recall']
-                if iouThr is not None:
-                    t = np.where(iouThr == p.iouThrs)[0]
-                    s = s[t]
-                s = s[:,:,aind,mind]
-            if len(s[s>-1])==0:
-                mean_s = -1
-            else:
-                mean_s = np.mean(s[s>-1])
-            print(iStr.format(titleStr, typeStr, iouStr, areaRng, maxDets, mean_s))
-            return mean_s
-
-        def _summarizeDets():
-            stats = np.zeros((25,))
-            stats[0] = _summarize(1)
-            stats[1] = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[2])
-            stats[2] = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[2])
-            stats[3] = _summarize(1, areaRng='small', maxDets=self.params.maxDets[2])
-            stats[4] = _summarize(1, areaRng='medium', maxDets=self.params.maxDets[2])
-            stats[5] = _summarize(1, areaRng='large', maxDets=self.params.maxDets[2])
-
-            stats[6] = _summarize(1, areaRng='0-8', maxDets=self.params.maxDets[2])
-            stats[7] = _summarize(1, areaRng='8-16', maxDets=self.params.maxDets[2])
-            stats[8] = _summarize(1, areaRng='16-24', maxDets=self.params.maxDets[2])
-            stats[9] = _summarize(1, areaRng='24-32', maxDets=self.params.maxDets[2])
-            stats[10] = _summarize(1, areaRng='32-48', maxDets=self.params.maxDets[2])
-            stats[11] = _summarize(1, areaRng='48-64', maxDets=self.params.maxDets[2])
-            stats[12] = _summarize(1, areaRng='64-80', maxDets=self.params.maxDets[2])
-            stats[13] = _summarize(1, areaRng='80-96', maxDets=self.params.maxDets[2])
-            stats[14] = _summarize(1, areaRng='96-128', maxDets=self.params.maxDets[2])
-            stats[15] = _summarize(1, areaRng='128-160', maxDets=self.params.maxDets[2])
-            stats[16] = _summarize(1, areaRng='160-192', maxDets=self.params.maxDets[2])
-            stats[17] = _summarize(1, areaRng='192-256', maxDets=self.params.maxDets[2])
-            stats[18] = _summarize(1, areaRng='256-', maxDets=self.params.maxDets[2])
-
-            stats[19] = _summarize(0, maxDets=self.params.maxDets[0])
-            stats[20] = _summarize(0, maxDets=self.params.maxDets[1])
-            stats[21] = _summarize(0, maxDets=self.params.maxDets[2])
-            stats[22] = _summarize(0, areaRng='small', maxDets=self.params.maxDets[2])
-            stats[23] = _summarize(0, areaRng='medium', maxDets=self.params.maxDets[2])
-            stats[24] = _summarize(0, areaRng='large', maxDets=self.params.maxDets[2])
-            return stats
-
-        if not self.eval:
-            raise Exception('Please run accumulate() first')
-        iouType = self.params.iouType
-        if iouType == 'segm' or iouType == 'bbox':
-            summarize = _summarizeDets
-        elif iouType == 'keypoints':
-            raise NotImplementedError
-        self.stats = summarize()
-
-    def __str__(self):
-        self.summarize()
-
-
-class ParamsCostumRanges:
-    '''
-    Params for coco evaluation api
-    '''
-    def setDetParams(self):
-        self.imgIds = []
-        self.catIds = []
-        # np.arange causes trouble.  the data point on arange is slightly larger than the true value
-        self.iouThrs = np.linspace(.5, 0.95, int(np.round((0.95 - .5) / .05)) + 1, endpoint=True)
-        self.recThrs = np.linspace(.0, 1.00, int(np.round((1.00 - .0) / .01)) + 1, endpoint=True)
-        self.maxDets = [1, 10, 100]
-        # self.areaRng = [[0 ** 2, 1e5 ** 2], [0 ** 2, 32 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
-        # self.areaRngLbl = ['all', 'small', 'medium', 'large']
-        self.areaRng = [
-            [0 ** 2, 1e5 ** 2], [0 ** 2, 32 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2],
-            [0 ** 2, 8 ** 2], [8 ** 2, 16 ** 2], [16 ** 2, 24 ** 2], [24 ** 2, 32 ** 2],
-            [32 ** 2, 48 ** 2], [48 ** 2, 64 ** 2], [64 ** 2, 80 ** 2], [80 ** 2, 96 ** 2],
-            [96 ** 2, 128 ** 2], [128 ** 2, 160 ** 2], [160 ** 2, 192 ** 2], [192 ** 2, 256 ** 2], [256 ** 2, 1e5 ** 2],
-        ]
-        self.areaRngLbl = [
-            'all', 'small', 'medium', 'large',
-            "0-8", "8-16", "16-24", "24-32",
-            "32-48", "48-64", "64-80", "80-96",
-            "96-128", "128-160", "160-192", "192-256", "256-"
-        ]
-        self.useCats = 1
-
-    def __init__(self, iouType='segm'):
-        if iouType == 'segm' or iouType == 'bbox':
-            self.setDetParams()
-        else:
-            raise Exception('iouType not supported')
-        self.iouType = iouType
-        # useSegm is deprecated
-        self.useSegm = None
